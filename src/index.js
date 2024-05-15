da530aeb3ca59069e70544d87fafb33056203d5c
dima <din.salikhov@gmail.com>
2024-05-15 17:34:55 +0300

    image.url

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ class Quote {
     let image = '';
     if (this.image) {
       image = `<div class="${this.rootClass}__image">
-        <img src="${this.image}" alt="" />
+        <img src="${this.image && this.image.url ? this.image.url : this.image}" alt="" />
         <button type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/></svg></button>
       </div>`
     } else {
